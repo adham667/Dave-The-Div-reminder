4315ff2e3a64b2fd68cdc9917332ff76dea6d819
adham667 <adhamsaeed667@gmail.com>
2024-10-03 21:04:56 +0300

    last update

--- index.mjs
+++ index.mjs
@@ -54,7 +54,8 @@ function scheduleReminders(contest, startDate) {
   const contestId = contest.id; // Use contest ID to track scheduled reminders
   const contestName = contest.name;
   const channel = client.channels.cache.find(channel => channel.name === 'div-reminders'); // Replace with your channel ID
-
+  console.log("hi");
+  
     // Check if the contest is tommorow
     const currentDate = new Date();
     const tomorrow = new Date(currentDate);
